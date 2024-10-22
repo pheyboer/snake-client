8a328579746ca7ea5031a7f6800b290b612b5fea
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-22 15:02:21 -0400

    removed unrelated test code

--- client.js
+++ client.js
@@ -14,18 +14,6 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully connected to the game server!');
     conn.write("Name: PRH");
-    
-    /*
-    conn.write("Move: up");
-
-    setTimeout(() => {
-      conn.write("Move: right");
-    }, 50);
-
-    setInterval(() => {
-      conn.write("Move: up");
-    }, 50);
-    */
   });
 
   // handle incoming data
