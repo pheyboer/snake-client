fe3debd4144bbb5336d0618ad0e8ad4bd4143f73
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-22 11:51:29 -0400

    added movement commands to console log

--- input.js
+++ input.js
@@ -12,6 +12,17 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit(); // CNTRL + C will exit game
   }
+
+  //handle movement commands
+  if (key === 'w') {
+    console.log("Move: up");
+  } else if (key === 's') {
+    console.log("Move: down");
+  } else if (key === 'a') {
+    console.log("Move: left");
+  } else if (key === 'd') {
+    console.log("Move: right");
+  }
 };
 
 module.exports = { setupInput };
