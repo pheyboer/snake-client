f271be4eebb6585fa88660ad736fb85377913030
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 23:26:50 -0400

    added function to handle user input and event listener

--- play.js
+++ play.js
@@ -6,7 +6,15 @@ const setupInput = function () {
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
   stdin.resume(); // resume stdin so the program can listen for input
+  stdin.on("data", handleUserInput); //event listener
   return stdin;   // return the stdin object so we can use it elsewhere in the program
+};
+
+//function to handle user input
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit(); // CNTRL + C will exit game
+  }
 };
 
 //handle error outside of connect function
@@ -20,3 +28,6 @@ const handleError = (conn) => {
 console.log("Connecting ...");
 const connection = connect(); //get connection info and hold in new variable
 handleError(connection); // pass connection info to handleError
+
+//calling setupInput
+setupInput();
