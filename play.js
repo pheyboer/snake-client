4d8fd00b1aa9948a997f4d4fe4baf62c34bcb51d
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 21:11:36 -0400

    added play.js with conn.on connection

--- play.js
+++ play.js
@@ -10,6 +10,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  //event listener for successful connection
+  conn.on('connect', () => {
+    console.log('Successfully connected to the server!');
+  });
+
   // handle incoming data
   conn.on("data", (data) => {
     console.log(data);
