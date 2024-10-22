5adf40b3840b0f56cc9641f13887ae2cbe49e9b3
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-22 12:25:57 -0400

    updated setupInput function

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+//stores the active TCP connection object. Defaults to undefined
+let connection; 
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
