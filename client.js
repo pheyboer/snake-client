77a3c7482de06755822d074cbd8d2128097cb1d3
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 21:32:53 -0400

    added client.js

---
+++ client.js
@@ -0,0 +1,22 @@
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  //event listener for successful connection
+  conn.on('connect', () => {
+    console.log('Successfully connected to the server!');
+  });
+
+  // handle incoming data
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
