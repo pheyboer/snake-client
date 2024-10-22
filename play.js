6596486c761e9fff77ddd2d88e90553418ac0d4c
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 21:06:30 -0400

    added play.js with conn.on connect and data with console log and comments

--- play.js
+++ play.js
@@ -10,12 +10,14 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  // message when connected
   conn.on("connect", (data) => {
    console.log("Connected");
   });
 
+  //message when idle
   conn.on("data", (data) => {
-    console.log("Server says: ", data);
+    console.log(data);
   });
 
   return conn;
