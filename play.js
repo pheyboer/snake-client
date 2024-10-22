9d571597d0183825e609f461b47d21bb378d7a6f
Peter Heyboer <heyboer.peter@gmail.com>
2024-10-21 22:46:32 -0400

    exporting and importing connect function

--- play.js
+++ play.js
@@ -1,27 +1,5 @@
-const net = require("net");
-
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  //event listener for successful connection
-  conn.on('connect', () => {
-    console.log('Successfully connected to the server!');
-  });
-
-  // handle incoming data
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 //handle error outside of connect function
 const handleError = (conn) => {
